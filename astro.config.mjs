0e1c9c80d53df9910fb4c77127069c5391c88ccc
MathewsAvlis <mathewsavlis@gmail.com>
2026-09-22 13:55:14 -0300

    chore: update production domain to ensaio.spozzer.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://sessao.spozzer.com",
+  site: "https://ensaio.spozzer.com",
 
   build: {
     inlineStylesheets: "always",
